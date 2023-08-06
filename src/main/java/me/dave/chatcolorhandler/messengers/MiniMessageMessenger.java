25d402e683b32b60ace35cf0b75283849079e084
BuildTools <cooldaveb@live.co.uk>
2023-08-06 07:03:42 +0100

    Added legacy support for MiniMessageMessenger

--- src/main/java/me/dave/chatcolorhandler/messengers/MiniMessageMessenger.java
+++ src/main/java/me/dave/chatcolorhandler/messengers/MiniMessageMessenger.java
@@ -4,6 +4,7 @@ import me.dave.chatcolorhandler.ChatColorHandler;
 import me.dave.chatcolorhandler.parsers.custom.MiniMessageParser;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,14 +14,13 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class MiniMessageMessenger extends AbstractMessenger {
-    private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     @Override
     public void sendMessage(@NotNull CommandSender recipient, @Nullable String message) {
         if (message == null || message.isBlank()) return;
 
         Audience audience = Audience.audience((Audience) recipient);
-        audience.sendMessage(miniMessage.deserialize(ChatColorHandler.translateAlternateColorCodes(message, (recipient instanceof Player player ? player : null), List.of(MiniMessageParser.class))));
+        audience.sendMessage(LegacyComponentSerializer.legacy('§').deserialize(ChatColorHandler.translateAlternateColorCodes(message, (recipient instanceof Player player ? player : null), List.of(MiniMessageParser.class))));
     }
 
     @Override
@@ -28,7 +28,7 @@ public class MiniMessageMessenger extends AbstractMessenger {
         if (message == null || message.isBlank()) return;
 
         Audience audience = Audience.audience((Audience) Bukkit.getOnlinePlayers());
-        audience.sendMessage(miniMessage.deserialize(message));
+        audience.sendMessage(LegacyComponentSerializer.legacy('§').deserialize(message));
     }
 
     @Override
@@ -36,6 +36,6 @@ public class MiniMessageMessenger extends AbstractMessenger {
         if (message == null || message.isBlank()) return;
 
         Audience audience = Audience.audience((Audience) player);
-        audience.sendActionBar(miniMessage.deserialize(message));
+        audience.sendActionBar(LegacyComponentSerializer.legacy('§').deserialize(message));
     }
 }
