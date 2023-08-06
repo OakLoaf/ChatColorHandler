c6cf14cc74e217135f38a94d945ac838be7d76cf
BuildTools <cooldaveb@live.co.uk>
2023-08-06 06:19:16 +0100

    Moved over to Paper's built in Audiences

--- src/main/java/me/dave/chatcolorhandler/messengers/MiniMessageMessenger.java
+++ src/main/java/me/dave/chatcolorhandler/messengers/MiniMessageMessenger.java
@@ -3,29 +3,23 @@ package me.dave.chatcolorhandler.messengers;
 import me.dave.chatcolorhandler.ChatColorHandler;
 import me.dave.chatcolorhandler.parsers.custom.MiniMessageParser;
 import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class MiniMessageMessenger extends AbstractMessenger {
-    private final BukkitAudiences adventure;
     private final MiniMessage miniMessage = MiniMessage.miniMessage();
-
-    public MiniMessageMessenger(Plugin plugin) {
-        this.adventure = BukkitAudiences.create(plugin);
-    }
 
     @Override
     public void sendMessage(@NotNull CommandSender recipient, @Nullable String message) {
         if (message == null || message.isBlank()) return;
 
-        Audience audience = adventure.sender(recipient);
+        Audience audience = Audience.audience((Audience) recipient);
         audience.sendMessage(miniMessage.deserialize(ChatColorHandler.translateAlternateColorCodes(message, (recipient instanceof Player player ? player : null), List.of(MiniMessageParser.class))));
     }
 
@@ -33,7 +27,7 @@ public class MiniMessageMessenger extends AbstractMessenger {
     public void broadcastMessage(@Nullable String message) {
         if (message == null || message.isBlank()) return;
 
-        Audience audience = adventure.all();
+        Audience audience = Audience.audience((Audience) Bukkit.getOnlinePlayers());
         audience.sendMessage(miniMessage.deserialize(message));
     }
 
@@ -41,7 +35,7 @@ public class MiniMessageMessenger extends AbstractMessenger {
     public void sendActionBarMessage(@NotNull Player player, @Nullable String message) {
         if (message == null || message.isBlank()) return;
 
-        Audience audience = adventure.player(player);
+        Audience audience = Audience.audience((Audience) player);
         audience.sendActionBar(miniMessage.deserialize(message));
     }
 }
