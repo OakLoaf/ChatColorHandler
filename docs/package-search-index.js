13ba1a6ae2ed955f7f40f9ca699d7b063577e251
BuildTools <cooldaveb@live.co.uk>
2023-08-07 08:07:48 +0100

    Added javadocs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.dave.chatcolorhandler"},{"l":"me.dave.chatcolorhandler.parsers"},{"l":"me.dave.chatcolorhandler.parsers.custom"}];updateSearchResults();
