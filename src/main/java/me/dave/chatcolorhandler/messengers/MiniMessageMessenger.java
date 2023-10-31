2020fcb04f29c8cf5b92e1b02312cfd4f5bca180
BuildTools <cooldaveb@live.co.uk>
2023-10-31 13:46:54 +0000

    Added sendTitle to MiniMessageMessenger

--- src/main/java/me/dave/chatcolorhandler/messengers/MiniMessageMessenger.java
+++ src/main/java/me/dave/chatcolorhandler/messengers/MiniMessageMessenger.java
@@ -7,12 +7,15 @@ import me.dave.chatcolorhandler.parsers.custom.PlaceholderAPIParser;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.Title;
+import net.kyori.adventure.title.TitlePart;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.time.Duration;
 import java.util.List;
 
 public class MiniMessageMessenger extends AbstractMessenger {
@@ -52,24 +55,30 @@ public class MiniMessageMessenger extends AbstractMessenger {
     public void sendTitle(@NotNull Player player, @Nullable String title) {
         if (title == null || title.isBlank()) return;
 
-        Audience audience = Audience.audience((Audience) player);
-        String legacyParsed = legacyParser(ChatColorHandler.translateAlternateColorCodes(title, player, List.of(LegacyCharParser.class, PlaceholderAPIParser.class)));
-        audience.sendActionBar(miniMessage.deserialize(legacyParsed));
+        sendTitle(player, title, null, 10, 70, 20);
     }
 
     @Override
     public void sendTitle(@NotNull Player player, @Nullable String title, @Nullable String subtitle) {
-
+        sendTitle(player, title, subtitle, 10, 70, 20);
     }
 
     @Override
     public void sendTitle(@NotNull Player player, @Nullable String title, @Nullable String subtitle, int fadeIn, int fadeOut) {
-
+        sendTitle(player, title, subtitle, fadeIn, 70, fadeOut);
     }
 
     @Override
     public void sendTitle(@NotNull Player player, @Nullable String title, @Nullable String subtitle, int fadeIn, int stay, int fadeOut) {
+        Audience audience = Audience.audience((Audience) player);
 
+        Title.Times times = Title.Times.times(Duration.ofMillis(fadeIn * 50L), Duration.ofMillis(stay * 50L), Duration.ofMillis(fadeOut * 50L));
+        String subtitleLegacyParsed = legacyParser(ChatColorHandler.translateAlternateColorCodes(subtitle, player, List.of(LegacyCharParser.class, PlaceholderAPIParser.class)));
+        String titleLegacyParsed = legacyParser(ChatColorHandler.translateAlternateColorCodes(title, player, List.of(LegacyCharParser.class, PlaceholderAPIParser.class)));
+
+        audience.sendTitlePart(TitlePart.TIMES, times);
+        audience.sendTitlePart(TitlePart.SUBTITLE, miniMessage.deserialize(subtitleLegacyParsed));
+        audience.sendTitlePart(TitlePart.TITLE, miniMessage.deserialize(titleLegacyParsed));
     }
 
     private String legacyParser(String string) {
