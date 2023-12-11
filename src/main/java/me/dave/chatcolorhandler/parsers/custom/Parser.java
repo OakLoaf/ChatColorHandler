1e036cba9fb5253c57c34c0da3eb922115a93750
BuildTools <cooldaveb@live.co.uk>
2023-12-11 17:34:39 +0000

    Removed unused comment

--- src/main/java/me/dave/chatcolorhandler/parsers/custom/Parser.java
+++ src/main/java/me/dave/chatcolorhandler/parsers/custom/Parser.java
@@ -5,5 +5,4 @@ import org.bukkit.entity.Player;
 public interface Parser {
     String parseString(String string);
     String parseString(String string, Player player);
-    // TODO: add stripColor method
 }
