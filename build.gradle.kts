c40edf61fdc91c8025835b0a410d47cd5a5e6949
davidbryce <cooldcb@gmail.com>
2024-01-31 22:51:26 +0000

    Moved to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version("8.1.1")
+}
+
+group = "me.dave"
+version = "v2.5.3"
+
+repositories {
+    mavenCentral() // MiniMessage, MiniPlaceholders
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") } // Spigot
+    maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") } // PlaceholderAPI
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("net.kyori:adventure-text-minimessage:4.15.0")
+    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3")
+    compileOnly("me.clip:placeholderapi:2.11.3")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    shadowJar {
+        archiveFileName.set("${project.name}-${project.version}.jar")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "smrt1Releases"
+            url = uri("https://repo.smrt-1.com/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "smrt1Snapshots"
+            url = uri("https://repo.smrt-1.com/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                isAllowInsecureProtocol = true
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
+            from(project.components["java"])
+        }
+    }
+}
