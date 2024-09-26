f68fa4668ec041493c1686276fcd97177c26d6b8
OakLoaf <oak@beaconstudios.org>
2024-09-26 11:30:08 +0100

    Moved initialising to ChatColorHandlerSettings

--- src/main/java/org/lushplugins/chatcolorhandler/ChatColorHandler.java
+++ src/main/java/org/lushplugins/chatcolorhandler/ChatColorHandler.java
@@ -1,16 +1,12 @@
 package org.lushplugins.chatcolorhandler;
 
-import org.lushplugins.chatcolorhandler.messengers.LegacyMessenger;
-import org.lushplugins.chatcolorhandler.messengers.MiniMessageMessenger;
 import org.lushplugins.chatcolorhandler.parsers.Parser;
 import org.lushplugins.chatcolorhandler.parsers.ParserTypes;
 import org.lushplugins.chatcolorhandler.parsers.Parsers;
-import org.lushplugins.chatcolorhandler.parsers.custom.*;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,40 +16,6 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class ChatColorHandler {
     private static final ChatColorHandlerSettings settings = new ChatColorHandlerSettings();
-
-    protected static void init() {
-        Parsers.register(LegacyHexParser.INSTANCE, 85);
-        Parsers.register(LegacySpigotParser.INSTANCE, 84);
-        Parsers.register(HexParser.INSTANCE, 83);
-
-        try {
-            Class.forName("net.kyori.adventure.text.minimessage.MiniMessage").getMethod("miniMessage");
-            settings.messenger(new MiniMessageMessenger());
-
-            Parsers.register(MiniMessageResolverParser.INSTANCE, 89);
-            Parsers.register(MiniMessageColorParser.INSTANCE, 73);
-            Parsers.register(MiniMessageInteractionParser.INSTANCE, 72);
-            Parsers.register(MiniMessagePlaceholderParser.INSTANCE, 71);
-            Parsers.register(MiniMessageTextFormattingParser.INSTANCE, 70);
-            debugLog("Found MiniMessage in Server. MiniMessage support enabled.");
-        } catch (ClassNotFoundException | NoSuchMethodException ignored) {
-            settings.messenger(new LegacyMessenger());
-            debugLog("Unable to find MiniMessage. MiniMessage support not enabled.");
-        }
-
-        PluginManager pluginManager = Bukkit.getServer().getPluginManager();
-        if (pluginManager.getPlugin("PlaceholderAPI") != null && pluginManager.isPluginEnabled("PlaceholderAPI")) {
-            Parsers.register(PlaceholderAPIParser.INSTANCE, 90);
-            debugLog("Found plugin \"PlaceholderAPI\". PlaceholderAPI support enabled.");
-        }
-
-        if (pluginManager.getPlugin("MiniPlaceholders") != null && pluginManager.isPluginEnabled("MiniPlaceholders")) {
-            Parsers.register(MiniPlaceholdersParser.INSTANCE, -1);
-            debugLog("Found plugin \"MiniPlaceholders\". MiniPlaceholders support enabled.");
-        }
-
-        settings.defaultParsers(ParserTypes.all());
-    }
 
     /**
      * Translates a string to allow for hex colours and placeholders
--- src/main/java/org/lushplugins/chatcolorhandler/ChatColorHandlerSettings.java
+++ src/main/java/org/lushplugins/chatcolorhandler/ChatColorHandlerSettings.java
@@ -1,20 +1,27 @@
 package org.lushplugins.chatcolorhandler;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.lushplugins.chatcolorhandler.messengers.LegacyMessenger;
 import org.lushplugins.chatcolorhandler.messengers.Messenger;
+import org.lushplugins.chatcolorhandler.messengers.MiniMessageMessenger;
 import org.lushplugins.chatcolorhandler.parsers.Parser;
 import org.lushplugins.chatcolorhandler.parsers.ParserTypes;
+import org.lushplugins.chatcolorhandler.parsers.Parsers;
+import org.lushplugins.chatcolorhandler.parsers.custom.*;
 
 import java.util.List;
 
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public class ChatColorHandlerSettings {
+    private boolean initialised = false;
     private Messenger messenger;
     private List<Parser> defaultParsers = ParserTypes.all();
     private boolean debug = false;
 
     public Messenger messenger() {
-        if (messenger == null) {
-            ChatColorHandler.init();
+        if (!initialised) {
+            init();
         }
 
         return messenger;
@@ -25,11 +32,19 @@ public class ChatColorHandlerSettings {
      * @param messenger The messenger to use
      */
     public ChatColorHandlerSettings messenger(Messenger messenger) {
+        if (!initialised) {
+            init();
+        }
+
         this.messenger = messenger;
         return this;
     }
 
     public List<Parser> defaultParsers() {
+        if (!initialised) {
+            init();
+        }
+
         return defaultParsers;
     }
 
@@ -52,5 +67,40 @@ public class ChatColorHandlerSettings {
     public ChatColorHandlerSettings debug(boolean debug) {
         this.debug = debug;
         return this;
+    }
+
+    private void init() {
+        Parsers.register(LegacyHexParser.INSTANCE, 85);
+        Parsers.register(LegacySpigotParser.INSTANCE, 84);
+        Parsers.register(HexParser.INSTANCE, 83);
+
+        try {
+            Class.forName("net.kyori.adventure.text.minimessage.MiniMessage").getMethod("miniMessage");
+            messenger(new MiniMessageMessenger());
+
+            Parsers.register(MiniMessageResolverParser.INSTANCE, 89);
+            Parsers.register(MiniMessageColorParser.INSTANCE, 73);
+            Parsers.register(MiniMessageInteractionParser.INSTANCE, 72);
+            Parsers.register(MiniMessagePlaceholderParser.INSTANCE, 71);
+            Parsers.register(MiniMessageTextFormattingParser.INSTANCE, 70);
+            ChatColorHandler.debugLog("Found MiniMessage in Server. MiniMessage support enabled.");
+        } catch (ClassNotFoundException | NoSuchMethodException ignored) {
+            messenger(new LegacyMessenger());
+            ChatColorHandler.debugLog("Unable to find MiniMessage. MiniMessage support not enabled.");
+        }
+
+        PluginManager pluginManager = Bukkit.getServer().getPluginManager();
+        if (pluginManager.getPlugin("PlaceholderAPI") != null && pluginManager.isPluginEnabled("PlaceholderAPI")) {
+            Parsers.register(PlaceholderAPIParser.INSTANCE, 90);
+            ChatColorHandler.debugLog("Found plugin \"PlaceholderAPI\". PlaceholderAPI support enabled.");
+        }
+
+        if (pluginManager.getPlugin("MiniPlaceholders") != null && pluginManager.isPluginEnabled("MiniPlaceholders")) {
+            Parsers.register(MiniPlaceholdersParser.INSTANCE, -1);
+            ChatColorHandler.debugLog("Found plugin \"MiniPlaceholders\". MiniPlaceholders support enabled.");
+        }
+
+        defaultParsers(ParserTypes.all());
+        initialised = true;
     }
 }
